11079adf72818c86cd49a66934bf577fa9a7a095
6710110151 <chinochi866@gmail.com>
2024-12-16 13:09:29 +0700

    blur bg for popup

--- src/components/Edititem.js
+++ src/components/Edititem.js
@@ -13,45 +13,49 @@ export default function Edititem({ onClose }) {
                         onClick={onClose}
                         className="close-edit" />
                     <div />
-                    <Form.Item
-                        name="type"
-                        label="ชนิด"
-                        rules={[{ required: true }]}
-                    >
-                        <Select
-                            allowClear
-                            style={{ width: "100px" }}
-                            options={[
-                                {
-                                    value: 'income',
-                                    label: 'รายรับ',
-                                },
-                                {
-                                    value: 'expense',
-                                    label: 'รายจ่าย',
-                                },
-                            ]}
-                        />
-                    </Form.Item>
-                    <Form.Item
-                        name="amount"
-                        label="จำนวนเงิน"
-                        rules={[{ required: true }]}>
-                        <InputNumber placeholder="จำนวนเงิน" />
-                    </Form.Item>
+                    <div className="edit-form">
+                        <Form.Item
+                            name="type"
+                            label="ชนิด"
+                            rules={[{ required: true }]}
+                        >
+                            <Select
+                                allowClear
+                                style={{ width: "100px" }}
+                                options={[
+                                    {
+                                        value: 'income',
+                                        label: 'รายรับ',
+                                    },
+                                    {
+                                        value: 'expense',
+                                        label: 'รายจ่าย',
+                                    },
+                                ]}
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="amount"
+                            label="จำนวนเงิน"
+                            rules={[{ required: true }]}>
+                            <InputNumber placeholder="จำนวนเงิน" />
+                        </Form.Item>
 
-                    <Form.Item
-                        name="note"
-                        label="หมายเหตุ"
-                        rules={[{ required: true }]}>
-                        <Input placeholder="Note" />
-                    </Form.Item>
-                    <Form.Item>
-                        <Button
-                            type="primary"
-                            htmlType="submit"
-                        >Submit</Button>
-                    </Form.Item>
+                        <Form.Item
+                            name="note"
+                            label="หมายเหตุ"
+                            rules={[{ required: true }]}>
+                            <Input placeholder="Note" />
+                        </Form.Item>
+                    </div>
+                    <div className="save-button">
+                        <Form.Item>
+                            <Button
+                                type="primary"
+                                htmlType="submit"
+                            >Save</Button>
+                        </Form.Item>
+                    </div>
                 </div>
             </div>
         </div>
