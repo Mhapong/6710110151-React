ccca4ce3acc9df1f647bb5ed2b9fd19931915955
6710110151 <chinochi866@gmail.com>
2024-12-18 20:41:29 +0700

    Add working Edit

--- src/components/Edititem.js
+++ src/components/Edititem.js
@@ -1,18 +1,17 @@
 import "../App";
 import React, { useEffect } from "react";
-import { Button, Form, Select, Input, InputNumber, X, Modal, } from 'antd';
+import { Button, Form, Select, Input, InputNumber, X, Modal } from "antd";
 
 const Edititem = ({ defaultValue, closeModal, onSave }) => {
     const [form] = Form.useForm();
-
 
     useEffect(() => {
         if (defaultValue) {
             form.setFieldsValue({
                 id: defaultValue.id,
+                type: defaultValue.type,
                 note: defaultValue.note,
-                amount: defaultValue.amount
-
+                amount: defaultValue.amount,
             });
         }
     }, [defaultValue, form]);
@@ -31,17 +30,15 @@ const Edititem = ({ defaultValue, closeModal, onSave }) => {
 
     return (
         <Modal
-            title='Edit transaction'
+            title="Edit transaction"
             open={true}
             onCancel={closeModal}
             footer={[
-                <Button
-                    type="primary"
-                    htmlType="submit"
-                    onClick={handleSave}
-                >Save</Button>
-
-            ]}>
+                <Button type="primary" key="submit" onClick={handleSave}>
+                    Save
+                </Button>,
+            ]}
+        >
             <div>
                 {/* <div className="close-edit">
                     <Button
@@ -50,39 +47,46 @@ const Edititem = ({ defaultValue, closeModal, onSave }) => {
                         onClick={closeModal}
                         className="close-edit" />
                 </div> */}
-                <Form>
+                <Form form={form} layout="vertical">
+                    <Form.Item name="id" label="ID" hidden>
+                        <Input disabled />
+                    </Form.Item>
+
                     <Form.Item
                         name="type"
                         label="ชนิด"
-                        rules={[{ required: true, message: 'Type must not empty' }]}
+                        rules={[{ required: true, message: "Type must not empty" }]}
                     >
-                        <div />
                         <Select
                             allowClear
                             style={{ width: "100px" }}
                             options={[
                                 {
-                                    value: 'income',
-                                    label: 'รายรับ',
+                                    value: "income",
+                                    label: "รายรับ",
                                 },
                                 {
-                                    value: 'expense',
-                                    label: 'รายจ่าย',
+                                    value: "expense",
+                                    label: "รายจ่าย",
                                 },
                             ]}
+                            rules={[{ required: true, message: "Type must not empty" }]}
                         />
                     </Form.Item>
+
                     <Form.Item
                         name="amount"
                         label="จำนวนเงิน"
-                        rules={[{ required: true, message: 'Amount must not empty' }]}>
+                        rules={[{ required: true, message: "Amount must not empty" }]}
+                    >
                         <InputNumber placeholder="จำนวนเงิน" />
                     </Form.Item>
 
                     <Form.Item
                         name="note"
                         label="หมายเหตุ"
-                        rules={[{ required: true, message: 'Note must not empty' }]}>
+                        rules={[{ required: true, message: "Note must not empty" }]}
+                    >
                         <Input.TextArea rows={1} />
                     </Form.Item>
                 </Form>
